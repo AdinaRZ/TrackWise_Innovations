34e0aac4b7bc48381b6c18e2ee609da2bdd616ef
Tatar Matei <tatarmateiccc@gmail.com>
2024-03-27 19:22:52 +0200

    Prototip aplicatie mobile / Interfata cu butoane si ceva cu firebase

---
+++ Modul 3/AplicatieIPbuna/app/src/main/java/com/example/aplicatieipbuna/Aplicatia.java
@@ -0,0 +1,17 @@
+
+package com.example.aplicatieipbuna;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Aplicatia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aplicatia);
+
+        // Aici poți adăuga logica specifică activității tale
+    }
+}
